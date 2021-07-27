6b09bcaead543b3a48bd352e0a7f14f6096117a8
KimUJin3359 <wj3359@naver.com>
2021-07-27 14:06:34 +0900

    Add Image Button Event

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,7 +3,7 @@
         <b-navbar id="nav-bar" style="font-family: 'Jua', sans-serif;" class="d-flex row-vw" toggleable="lg" variant="faded" type="light">
             <!-- Right aligned nav items -->
             <b-navbar-nav id="right-side" class="ml-auto">
-                <img id="logo" src="@/assets/alarm.png" alt="알람_로고">
+                <img id="logo" src="@/assets/alarm.png" alt="알람_로고" @click="print">
                 <img id="logo" src="@/assets/mail.png" alt="알람_로고">
                 <b-button id="button" variant="faded" type="light" size="lg">내 정보 보기</b-button>
                 <b-button id="button" variant="faded" type="light" size="lg">로그아웃</b-button>
@@ -11,6 +11,17 @@
         </b-navbar>
     </div>
 </template>
+
+<script>
+export default({
+    methods: {
+        print() {
+            console.log('alarm');
+        }
+    }
+})
+</script>
+
 
 <style>
 #nav-bar {
@@ -29,9 +40,10 @@
 }
 
 #nav-bar #logo {
+    margin-top: 5px;
     max-height: 40px;
     max-width: 40px;
-    margin-right: 20px;
+    margin-right: 30px;
 }
 
 </style>
