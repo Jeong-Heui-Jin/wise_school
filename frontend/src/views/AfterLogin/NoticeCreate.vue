e211131c2f99c76171747bf15790a920055fb9aa
meantint <mistinca@naver.com>
2021-07-29 10:38:08 +0900

    Add Skeleton Code

---
+++ frontend/src/views/AfterLogin/NoticeCreate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NavSideBar />
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavSideBar from "@/components/NavSideBarTeacher.vue";
+import NavBar from "@/components/NavBar.vue";
+export default {
+  name: "NoticeCreate",
+  components: {
+    NavSideBar,
+    NavBar,
+  },
+};
+</script>
+
+<style>
+</style>
