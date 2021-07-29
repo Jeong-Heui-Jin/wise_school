c4ab8e60b38d8e4366aa4f8c2c5e91163bff2d24
meantint <mistinca@naver.com>
2021-07-29 14:32:33 +0900

    Create NoticeCreate.vue file

--- frontend/src/views/AfterLogin/NoticeCreate.vue
+++ frontend/src/views/AfterLogin/NoticeCreate.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
+  <div style="font-family: 'Jua', sans-serif" variant="light">
     <NavSideBar />
     <NavBar />
+
+    <h1 id="noticeCreateTitle">공지사항 작성</h1>
+
+    <div id="createForm">
+      <h3>
+        <button id="cancelBtn">취소</button>
+        <button id="saveBtn">저장</button>
+      </h3>
+    </div>
   </div>
 </template>
 
@@ -18,4 +27,47 @@ export default {
 </script>
 
 <style>
+#cancelBtn {
+  position: absolute;
+  top: 90%;
+  left: 36%;
+
+  border-radius: 12px;
+
+  color: red;
+  background-color: #f5a8a8;
+
+  min-width: 11%;
+}
+
+#saveBtn {
+  position: absolute;
+  top: 90%;
+  left: 53%;
+
+  border-radius: 12px;
+
+  color: rgb(45, 45, 255);
+  background-color: #9a9cff;
+
+  min-width: 11%;
+}
+
+#noticeCreateTitle {
+  position: absolute;
+  left: 22%;
+}
+
+#createForm {
+  position: absolute;
+
+  min-width: 70%;
+  min-height: 550px;
+
+  top: 112px;
+  left: 25%;
+
+  background-color: rgb(157, 230, 157);
+  border-radius: 10px;
+}
 </style>
