18bf8350d84d90cf4b33d58d3d0443596db13e87
Jeong-Heui-Jin <sksgmlwlsl34@gmail.com>
2021-08-02 16:08:57 +0900

    Update student_manage

--- 슬학생_back/student_manage/urls.py
+++ 슬학생_back/student_manage/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'student_manage'
 
 urlpatterns = [
-    path('attendance/<int:class_id>/<str:date>/', views.attendance_list),
+    path('attendance/<int:class_id>/', views.attendance_list),
     path('attendance/detail/<int:student_id>/<str:date>/', views.attendance_detail),
     path('attendance-change/<int:student_id>/<str:date>/', views.attendance_change),
     path('note/<int:student_id>/', views.note_list),
