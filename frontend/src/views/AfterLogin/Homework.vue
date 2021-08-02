e347025bed8b9909b8aac3f4d943b2617c2275d1
meantint <mistinca@naver.com>
2021-08-02 13:20:13 +0900

    Create Homework.vue fields data

--- frontend/src/views/AfterLogin/Homework.vue
+++ frontend/src/views/AfterLogin/Homework.vue
@@ -5,8 +5,28 @@
 
     <h1>숙제 검사</h1>
 
-    <!-- 번호, 숙제 제목, 종료일, 제출 -->
-    <b-table id="homeworkForm" hover :items="items"></b-table>
+    <!-- 설정 목록
+    :hover -> 마우스 포인터를 어디 올렸는지 표시
+    :small -> table의 사이즈를 작게할 것인지
+    :borderless -> border 없앰
+    :items -> items에 있는 모든 데이터를 가져온다.
+    -->
+    <b-table
+      id="homeworkForm"
+      :hover="true"
+      :small="false"
+      :borderless="true"
+      :items="items"
+      :fields="fields"
+    >
+      <template #cell(index)="data">
+        {{ data.index + 1 }}
+      </template>
+
+      <template #cell(items)="data">
+        {{ data.items }}
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -18,6 +38,12 @@ export default {
   name: "Homework",
   data() {
     return {
+      fields: [
+        { key: "index", label: "번호" },
+        { key: "homeworkTitle", label: "숙제 제목" },
+        { key: "endDate", label: "종료일" },
+        { key: "submitInfo", label: "제출" },
+      ],
       items: [
         {
           homeworkTitle: "수학익힘책 16쪽 풀기",
@@ -51,7 +77,7 @@ export default {
   max-width: 70%;
   min-width: 70%;
   max-height: 75%;
-  min-height: 75%;
+  /* min-height: 75%; */
 
   top: 112px;
   left: 25%;
