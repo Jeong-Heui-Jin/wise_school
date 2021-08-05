3b24b4b8ac2d83ac43798b7c79101057008b13ab
meantint <mistinca@naver.com>
2021-08-05 16:23:51 +0900

    Create data.items.Content

--- frontend/src/views/AfterLogin/Homework.vue
+++ frontend/src/views/AfterLogin/Homework.vue
@@ -1,64 +1,51 @@
 <template>
-  <div
-    class="fixed-top d-flex align-items-center justify-content-center"
-    id="homeworkBaseForm"
-    style="font-family: 'Jua', sans-serif"
-    variant="light"
-  >
-    <!-- <NavSideBar />
-    <NavBar /> -->
+  <div style="font-family: 'Jua', sans-serif" variant="light">
+    <NavSideBar />
+    <NavBar />
 
-    <h1 id="title">숙제 검사</h1>
+    <h1 id="homeworkTitle">숙제 검사</h1>
 
-    <div>
-      <!-- Homework Create Button -->
-      <button id="homeworkCreateBtn" @click="goHomeworkCreate()">
-        숙제 추가하기
-      </button>
+    <!-- Homework Create Button -->
+    <button id="homeworkCreateBtn" @click="goHomeworkCreate()">
+      숙제 추가하기
+    </button>
 
-      <!-- table/button/pagination div -->
-      <div id="homeworkForm">
-        <b-table
-          id="homeworkTable my-table"
-          :hover="true"
-          :small="false"
-          :borderless="true"
-          :items="items"
-          :fields="fields"
-          :per-page="perPage"
-          :current-page="currentPage"
-        >
-          <!-- 번호 column -->
-          <!-- <template #cell(index)="data">
-          {{ data.index + 1 }}
-        </template> -->
+    <!-- table/button/pagination div -->
+    <div id="homeworkForm">
+      <b-table
+        id="homeworkTable my-table"
+        :hover="true"
+        :small="false"
+        :borderless="true"
+        :items="items"
+        :fields="fields"
+        :per-page="perPage"
+        :current-page="currentPage"
+        @row-clicked="goHomeworkView"
+      >
+        <!-- items column -->
+        <template #cell(items)="data">
+          <b-link>{{ data.items }}</b-link>
+        </template>
+      </b-table>
 
-          <!-- items column -->
-          <template #cell(items)="data">
-            {{ data.items }}
-          </template>
-        </b-table>
-
-        <!-- Pagination -->
-        <b-pagination
-          id="paginationForm"
-          pills
-          v-model="currentPage"
-          :total-rows="rows"
-          :per-page="perPage"
-          aria-controls="my-table"
-          align="center"
-        ></b-pagination>
-      </div>
+      <!-- Pagination -->
+      <b-pagination
+        id="paginationForm"
+        pills
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+        align="center"
+      ></b-pagination>
     </div>
-
-    <div></div>
   </div>
 </template>
 
 <script>
-// import NavSideBar from "@/components/NavSideBarTeacher.vue";
-// import NavBar from "@/components/NavBar.vue";
+import NavSideBar from "@/components/NavSideBarTeacher.vue";
+import NavBar from "@/components/NavBar.vue";
 
 export default {
   name: "Homework",
@@ -69,123 +56,149 @@ export default {
       fields: [
         // Title name 변경
         { key: "homework_id", label: "번호" },
-        { key: "Title", label: "숙제 제목" },
-        { key: "EndTime", label: "종료일" },
+        { key: "title", label: "숙제 제목" },
+        { key: "endTime", label: "종료일" },
         { key: "submitInfo", label: "제출" },
       ],
       items: [
         {
           homework_id: "17",
-          Title: "수학익힘책 16쪽 풀기",
-          EndTime: "7.19(월)",
+          title: "수학익힘책 16쪽 풀기",
+          endTime: "7.19(월)",
           submitInfo: "1/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "16",
-          Title: "어린왕자 읽기",
-          EndTime: "7.16(금)",
+          title: "어린왕자 읽기",
+          endTime: "7.16(금)",
           submitInfo: "4/6",
+          Content: "어린왕자 읽기",
         },
         {
           homework_id: "15",
-          Title: "알파벳 10번씩 쓰기",
-          EndTime: "7.16(금)",
+          title: "알파벳 10번씩 쓰기",
+          endTime: "7.16(금)",
           submitInfo: "3/6",
+          Content: "알파벳 10번씩 쓰기",
         },
         {
           homework_id: "14",
-          Title: "한자 15000번 쓰기",
-          EndTime: "7.15(목)",
+          title: "한자 15000번 쓰기",
+          endTime: "7.15(목)",
           submitInfo: "2/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "13",
-          Title: "기하와 벡터 3페이지 풀어오기",
-          EndTime: "7.14(수)",
+          title: "기하와 벡터 3페이지 풀어오기",
+          endTime: "7.14(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "12",
-          Title: "만화책 3권 읽고 오기",
-          EndTime: "7.14(수)",
+          title: "만화책 3권 읽고 오기",
+          endTime: "7.14(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "11",
-          Title: "아이유 노래 1곡 듣기",
-          EndTime: "7.14(수)",
+          title: "아이유 노래 1곡 듣기",
+          endTime: "7.14(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "10",
-          Title: "숙제 10",
-          EndTime: "7.13(수)",
+          title: "숙제 10",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "9",
-          Title: "숙제 9",
-          EndTime: "7.13(수)",
+          title: "숙제 9",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "8",
-          Title: "숙제 8",
-          EndTime: "7.13(수)",
+          title: "숙제 8",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "7",
-          Title: "숙제 7",
-          EndTime: "7.13(수)",
+          title: "숙제 7",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "6",
-          Title: "숙제 6",
-          EndTime: "7.13(수)",
+          title: "숙제 6",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "5",
-          Title: "숙제 5",
-          EndTime: "7.13(수)",
+          title: "숙제 5",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "4",
-          Title: "숙제 4",
-          EndTime: "7.13(수)",
+          title: "숙제 4",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "3",
-          Title: "숙제 3",
-          EndTime: "7.13(수)",
+          title: "숙제 3",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "2",
-          Title: "숙제 2",
-          EndTime: "7.13(수)",
+          title: "숙제 2",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
         {
           homework_id: "1",
-          Title: "숙제 1",
-          EndTime: "7.13(수)",
+          title: "숙제 1",
+          endTime: "7.13(수)",
           submitInfo: "6/6",
+          Content: "수학익힘책 16쪽 풀기",
         },
       ],
     };
   },
   components: {
-    // NavSideBar,
-    // NavBar,
+    NavSideBar,
+    NavBar,
   },
   methods: {
     goHomeworkCreate: function () {
       window.open("/homework_create", "_self");
+    },
+    goHomeworkView: function (value) {
+      console.log(value);
+      // router.push({
+      //   path: "/homework_view",
+      //   query: { title: this.items.Title, Content: this.items.Content },
+      // });
+      // setTimeout(() => console.log("after"), 30000); // test
+      // window.open("/homework_view", "_self");
     },
   },
   computed: {
@@ -197,18 +210,14 @@ export default {
 </script>
 
 <style>
-#homeworkBaseForm {
-  background-color: white;
-}
-
-#homeworkBaseForm #title {
+#homeworkTitle {
   position: fixed;
   top: 10px;
-  /* left: 22%; */
+  left: 120px;
 }
 
 #homeworkForm {
-  /* position: fixed; */
+  position: fixed;
 
   /* 최대 가로/세로 길이 설정 */
   /* 너비는 최소/최대 길이 동일 설정 */
@@ -217,8 +226,8 @@ export default {
   height: 600px;
   /* min-height: 75%; */
 
-  top: 112px;
-  left: 350px;
+  top: 152px;
+  left: 400px;
 
   /* font-size 증가 */
   font-size: 130%;
@@ -230,9 +239,9 @@ export default {
 }
 
 #homeworkCreateBtn {
-  /* position: absolute;
+  position: absolute;
   top: 112px;
-  left: 1200px; */
+  left: 1200px;
 
   min-width: 155px;
 
