db9cc39ed493aaa8e2e0fe60fd56b8a538f63f2c
jmj961103 <jmj961103@naver.com>
2021-08-13 13:48:36 +0900

    update App.vue: function getAttendance and [BE] setting.py

--- backend/SHS/settings.py
+++ backend/SHS/settings.py
@@ -4,6 +4,7 @@ import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_SSL_REDIRECT = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
